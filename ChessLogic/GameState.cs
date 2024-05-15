65b47035d98577e3a042c8a6c96d18f8727bbc85
Kayurov Dmytro <e1337ajke@gmail.com>
2024-05-15 12:36:11 +0300

    Added classes for all pieces. Add classes Board and GameState. The basic logic for forming pieces on the board is prescribed.

---
+++ ChessLogic/GameState.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessLogic
+{
+    public class GameState
+    {
+        public Board Board { get; }
+        public Player CurrentPlayer { get; private set; }
+
+        public GameState(Player player, Board board)
+        {
+            CurrentPlayer = player;
+            Board = board;
+        }
+    }
+}
