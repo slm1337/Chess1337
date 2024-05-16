1e064328857d01275ef440efc4740e06c3da6fd6
Kayurov Dmytro <e1337ajke@gmail.com>
2024-05-16 20:41:08 +0300

    Added PawnPromotion

--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -43,15 +43,32 @@
             return board[pos].Color != Color;
         }
 
+        private static IEnumerable<Move> PromotionMoves(Position from, Position to)
+        {
+            yield return new PawnPromotion(from, to, PieceType.Knight);
+            yield return new PawnPromotion(from, to, PieceType.Bishop);
+            yield return new PawnPromotion(from, to, PieceType.Rook);
+            yield return new PawnPromotion(from, to, PieceType.Queen);
+        }
+
+
         private IEnumerable<Move> ForwardMoves(Position from, Board board)
         {
-            
             Position oneMovePos = from + forward;
-                
 
             if (CanMoveTo(oneMovePos, board))
             {
-                yield return new NormalMove(from, oneMovePos);
+                if (oneMovePos.Row == 0 || oneMovePos.Row == 7)
+                {
+                    foreach (Move promMove in PromotionMoves(from, oneMovePos))
+                    {
+                        yield return promMove;
+                    }
+                }
+                else
+                {
+                    yield return new NormalMove(from, oneMovePos);
+                }
 
                 Position twoMovesPos = oneMovePos + forward;
 
@@ -68,14 +85,25 @@
             {
                 Position to = from + forward + dir;
 
-                if (CanCaptureAt(to, board)) {
-                    yield return new NormalMove(from, to);
+                if (CanCaptureAt(to, board))
+                {
+                    if (to.Row == 0 || to.Row == 7)
+                    {
+                        foreach (Move promMove in PromotionMoves(from, to))
+                        {
+                            yield return promMove;
+                        }
+                    }
+                    else
+                    {
+                        yield return new NormalMove(from, to);
+                    }
                 }
             }
         }
 
-        public override IEnumerable<Move> GetMoves(Position from, Board board) 
-        { 
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
             return ForwardMoves(from, board).Concat(DiagonalMoves(from, board));
         }
 
